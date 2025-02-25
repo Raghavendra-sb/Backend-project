f1a48358743adb3108b9a550277629c2105ddad5
Raghavendra-sb <raghavendra.sb.07@gmail.com>
2025-02-25 07:50:08 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,32 @@
+// import mongoose  from "mongoose";
+// import { DB_NAME } from "../constants.js";
+
+// const connectDB= async()=>
+// {
+//     try {
+//        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//        console.log(`Connection MongoDB done : DB HOST , ${connectionInstance.connection.host}`)
+        
+//     } catch (error) {
+//         console.error(`Error connecting mongosDB ${error}`);
+//         process.exit(1);
+//     }
+// }
+
+// export default connectDB;
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const mongoURI = `${process.env.MONGODB_URI}/${DB_NAME}`;
+        console.log(`Attempting to connect with URI: ${mongoURI}`); // Debug line
+        const connectionInstance = await mongoose.connect(mongoURI);
+        console.log(`Connection to MongoDB successful! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.error(`Error connecting to MongoDB: ${error}`);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
