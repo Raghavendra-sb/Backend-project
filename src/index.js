30fbfe5df951f404d974548583af2efc3af8c193
Raghavendra-sb <raghavendra.sb.07@gmail.com>
2025-02-27 18:20:13 +0530

    handle API response and errors

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@
 import dotenv from "dotenv"
 
 import connectDB from "./db/index.js";  // 👈 Add filename explicitly
+import app from "./app.js";
 dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+   app.listen(process.env.PORT || 8000 ,()=>{
+    console.log(`Server running on the port ${process.env.PORT}`);
+    app.on("error",(error)=>{
+        console.log("Error:",error);
+        throw error; 
+    })
+   })
+})
+.catch((error)=>{
+ console.error(`Mongo db connection failed :${error}`)
+})
 
 
 
---
+++ src/utils/ApiError.js
@@ -0,0 +1,27 @@
+// This is a JavaScript class called ApiError that extends (builds on top of) the built-in Error class. The Error class is what JavaScript uses to represent errors—like when something crashes or fails. By extending it, ApiError inherits all the basic features of Error (like a message and a stack trace) and adds extra properties to make it more useful for specific scenarios, such as handling errors in an API.
+class ApiError extends Error
+{
+    constructor(
+        statusCode,
+        message="something went wrong",
+        errors = [],
+        stack= ""
+
+    )
+    {
+        super(message) //super(message): Calls the parent Error class’s constructor with the message. This sets up the basic error message (e.g., "Not found") that Error already knows how to handle.
+        this.statusCode=statusCode
+        this.data=null
+        this.message=message
+        this.success=false;
+        this.errors=errors
+
+        if(stack)
+        {
+            this.stack=stack
+        }
+        else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}export {ApiError}
